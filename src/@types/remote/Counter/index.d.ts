61391a6a2e681441b302e55e5d347642ca2c7ebf
Piotr Gulbis <pgulbis@gmail.com>
2022-03-15 01:08:12 +0100

    add typescript config

---
+++ src/@types/remote/Counter/index.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export type Counter = React.FunctionComponent<{ count: number }>;
+
+export default true;
--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,38 @@ import Counter from "remote/Counter";
 
 import "./index.css";
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <div style={{ fontSize: "1rem", color: "red" }}>Something went wrong.</div>;
+    }
+    return this.props.children;
+  }
+}
+
 const App = () => (
   <div className="container">
-    <div style={{marginBottom: "8px"}}>Name: host</div>
-    <Counter />
+    <div style={{ marginBottom: "8px" }}>Name: host</div>
+    <ErrorBoundary>
+      <Counter name="Host" />
+    </ErrorBoundary>
   </div>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));
